a28c0a4d365ab814ff898218a2d2c92043c0b297
epkisiel <uT_4002@zsp3zamosc.pl>
2026-02-12 10:22:39 +0100

    dokonczone

--- FormularzRejestracjiNaWydarzenie/MainPage.xaml.cs
+++ FormularzRejestracjiNaWydarzenie/MainPage.xaml.cs
@@ -13,40 +13,75 @@
             InitializeComponent();
             RadioButtonBasic.IsChecked = true;
             DataPicker.MinimumDate = DateTime.Today;
-            DataPicker.Date = new DateTime(2026,2,16);
+            DataPicker.Date = new DateTime(2026,6,16);
         }
 
         private void LiczCene()
         {
+            /********************************
+            nazwa funkcji: LiczCene
+            opis funkcji: liczy cenę całkowitą
+            paramerty: brak
+            zwracany typ i opis: brak
+            autor: E.P.
+            *********************************/
+
             cena = cenaWybranegoPakietu + 100 * liczbaDodatkowychOsob;
             CenaCalkowita.Text = "Cena całkowita: " + cena + " zł";
         }
 
         private void Data_DateSelected(object sender, DateChangedEventArgs e)
         {
+            /********************************
+            nazwa funkcji: Data_DateSelected
+            opis funkcji: Sprawdza czy wybrana data jest dostępna
+            paramerty: sender, e
+            zwracany typ i opis: brak
+            autor: E.P.
+            *********************************/
+
             var dataPicker1 = DataPicker.Date.ToString();
             string dataPicker = dataPicker1.Substring(0, dataPicker1.Length - 9);
 
-            if(dataPicker!= "16.02.2026" &&  dataPicker!= "18.02.2026" && dataPicker!= "23.02.2026" && dataPicker!=null && dataPicker!="")
+            if(dataPicker== "16.06.2026" ||  dataPicker== "18.06.2026" || dataPicker== "23.06.2026" )
             {
-                DataInfoLabel.IsVisible = true;
-                poprawnaData = false;
+                poprawnaData = true;
+                DataInfoLabel.TextColor = Colors.Black;
+                DataInfoLabel.Text = "Wybrana data: " + dataPicker;
             }
             else
             {
-                poprawnaData= true;
-                DataInfoLabel.IsVisible=false;
+                DataInfoLabel.TextColor = Colors.Red;
+                DataInfoLabel.Text = "Termin niedostępny";
+                poprawnaData = false;
             }
         }
 
         private void DodatkoweOsobyStepper_ValueChanged(object sender, ValueChangedEventArgs e)
         {
-            liczbaDodatkowychOsob=(int)DodatkoweOsobyStepper.Value;
+            /********************************
+            nazwa funkcji: DodatkoweOsobyStepper_ValueChanged
+            opis funkcji: pobiera liczbę osób dodatkowych i wywołuje funkcję LiczCene
+            paramerty: sender, e
+            zwracany typ i opis: brak
+            autor: E.P.
+            *********************************/
+
+            liczbaDodatkowychOsob = (int)DodatkoweOsobyStepper.Value;
+            DodatkoweOsobyLabel.Text = "Osoby dodatkowe: " + liczbaDodatkowychOsob;
             LiczCene();
         }
 
         private void RadioButton_CheckedChanged(object sender, CheckedChangedEventArgs e)
         {
+            /********************************
+            nazwa funkcji: RadioButton_CheckedChanged
+            opis funkcji: sprawdza, który pakiet został wybrany i wywołuje funkcję LiczCene
+            paramerty: sender, e
+            zwracany typ i opis: brak
+            autor: E.P.
+            *********************************/
+
             if (e.Value)
             {
                 var radioButton = (RadioButton)sender;
@@ -69,7 +104,15 @@
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-            if(ImieINazwiskoEntry.Text!=null && EmailEntry.Text!=null && ImieINazwiskoEntry.Text != "" && EmailEntry.Text != "" && FirmaPicker.SelectedIndex!=-1 && poprawnaData)
+            /********************************
+            nazwa funkcji: Button_Clicked
+            opis funkcji: sprawdza, czy wprowadzone dane są poprawne i wyświetla komunikat z podsumowaniem
+            paramerty: sender, e
+            zwracany typ i opis: brak
+            autor: E.P.
+            *********************************/
+
+            if (ImieINazwiskoEntry.Text!=null && EmailEntry.Text!=null && ImieINazwiskoEntry.Text != "" && EmailEntry.Text != "" && FirmaPicker.SelectedIndex!=-1 && poprawnaData==true)
             {
                 string imieINazwisko=ImieINazwiskoEntry.Text;
                 string email = EmailEntry.Text;
